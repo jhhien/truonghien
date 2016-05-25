d4d8963b011793b9c8832bf92451833f50c216e6
jhhien <jhhien@gmail.com>
2016-05-25 16:21:05 +0700

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,15 @@
+<div id="footer">
+    <!-- Please leave this line intact -->
+    <p>Template design <a href="http://google.com.vn">Six Shooter Media</a><br />
+    <!-- you can delete below here -->
+    <?php the_time('Y'); ?> <?php bloginfo('name'); ?><br />
+    <a href="<?php bloginfo('rss2_url'); ?>">Grab the feed</a>
+    </p>
+</div>
+
+</div>
+
+<?php  wp_footer(); ?>
+
+</body>
+</html>
